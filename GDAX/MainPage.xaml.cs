b35e63ab799f9159f2e3b157a07eb7a6efd4b683
Ismael <ismael.em@outlook.com>
2017-08-16 22:39:10 +0200

    Polished the UI, and fixed some texts malfunctioning.

--- GDAX/MainPage.xaml.cs
+++ GDAX/MainPage.xaml.cs
@@ -1,15 +1,31 @@
-﻿using System.Diagnostics;
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+using Windows.UI.Xaml.Media;
 
 namespace GDAX {
-
     public sealed partial class MainPage : Page {
 
         public MainPage() {
             this.InitializeComponent();
+
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationView view = ApplicationView.GetForCurrentView();
+            ApplicationViewTitleBar titleBar = view.TitleBar;
+
+            titleBar.BackgroundColor = Color.FromArgb(0, 0, 0, 0);
+            titleBar.ForegroundColor = Color.FromArgb(0, 0, 0, 0);
+            titleBar.InactiveBackgroundColor = Color.FromArgb(0, 0, 0, 0);
+            titleBar.InactiveForegroundColor = Color.FromArgb(0, 0, 0, 0);
+
+            titleBar.ButtonBackgroundColor = Color.FromArgb(0, 0, 0, 0);
+            titleBar.ButtonHoverBackgroundColor = Color.FromArgb(100, 10, 10, 10);
+            //titleBar.ButtonPressedBackgroundColor = Color.FromArgb(0, 20, 20, 20);
+            titleBar.ButtonInactiveBackgroundColor = Color.FromArgb(0, 255, 0, 255);
+            titleBar.ButtonInactiveForegroundColor = Color.FromArgb(0, 0, 0, 0);
+
             MainFrame.Navigate(typeof(Page2));
         }
 
--- GDAX/Page2.xaml.cs
+++ GDAX/Page2.xaml.cs
@@ -4,16 +4,24 @@ using System.Threading.Tasks;
 using Telerik.UI.Xaml.Controls.Chart;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace GDAX {
     public sealed partial class Page2 : Page {
 
+        internal static int granularity = 3600;
+        internal static int num;
+
         public class PricePoint {
             public string Time { get; set; }
             public float Value { get; set; }
+        }
+
+        private int _BaudRate = 250;
+        public int BaudRate {
+            get { return _BaudRate; }
+            set { _BaudRate = 250; }
         }
 
         public class ChartDataObject {
@@ -61,54 +69,56 @@ namespace GDAX {
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         async private Task UpdateBTC() {
             await App.GetData("BTC-EUR");
-            BTC_curr.Text = "Current price: " + App.currency_BTC.ToString();
+            BTC_curr.Text = "Current price: " + App.currency_BTC.ToString() + "€";
 
-            await App.GetHistoricValues(3600, "BTC-EUR");
+            await App.GetHistoricValues(granularity, "BTC-EUR");
 
             List<ChartDataObject> data = UpdateChartContent();
 
-            Telerik.UI.Xaml.Controls.Chart.LineSeries series = (Telerik.UI.Xaml.Controls.Chart.LineSeries)BTC_Chart.Series[0];
+            LineSeries series = (LineSeries)BTC_Chart.Series[0];
             series.CategoryBinding = new PropertyNameDataPointBinding() { PropertyName = "Date" };
-            series.ValueBinding = new PropertyNameDataPointBinding() { PropertyName = "Value" };
+            series.ValueBinding    = new PropertyNameDataPointBinding() { PropertyName = "Value" };
 
             series.ItemsSource = data;
         }
         async private Task UpdateETH() {
             await App.GetData("ETH-EUR");
-            ETH_curr.Text = "Current price: " + App.currency_ETH.ToString();
+            ETH_curr.Text = "Current price: " + App.currency_ETH.ToString() + "€";
 
-            await App.GetHistoricValues(3600, "ETH-EUR");
-
-            //(ETH_Chart.Series[0] as WinRTXamlToolkit.Controls.DataVisualization.Charting.LineSeries).ItemsSource = LoadChartContents();
+            await App.GetHistoricValues(granularity, "ETH-EUR");
+            
             List<ChartDataObject> data = UpdateChartContent();
 
-            Telerik.UI.Xaml.Controls.Chart.LineSeries series = (Telerik.UI.Xaml.Controls.Chart.LineSeries)ETH_Chart.Series[0];
+            LineSeries series = (LineSeries)ETH_Chart.Series[0];
             series.CategoryBinding = new PropertyNameDataPointBinding() { PropertyName = "Date" };
-            series.ValueBinding = new PropertyNameDataPointBinding() { PropertyName = "Value" };
+            series.ValueBinding    = new PropertyNameDataPointBinding() { PropertyName = "Value" };
 
             series.ItemsSource = data;
         }
 
         async private Task UpdateLTC() {
             await App.GetData("LTC-EUR");
-            LTC_curr.Text = "Current price: " + App.currency_LTC.ToString();
+            LTC_curr.Text = "Current price: " + App.currency_LTC.ToString() + "€";
 
-            await App.GetHistoricValues(3600, "LTC-EUR");
+            await App.GetHistoricValues(granularity, "LTC-EUR");
 
             List<ChartDataObject> data = UpdateChartContent();
 
-            Telerik.UI.Xaml.Controls.Chart.LineSeries series = (Telerik.UI.Xaml.Controls.Chart.LineSeries)LTC_Chart.Series[0];
+            LineSeries series = (LineSeries)LTC_Chart.Series[0];
             series.CategoryBinding = new PropertyNameDataPointBinding() { PropertyName = "Date" };
-            series.ValueBinding = new PropertyNameDataPointBinding() { PropertyName = "Value" };
+            series.ValueBinding    = new PropertyNameDataPointBinding() { PropertyName = "Value" };
 
             series.ItemsSource = data;
         }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         private List<ChartDataObject> UpdateChartContent() {
+
+            num = 200;
+
             List<ChartDataObject> data = new List<ChartDataObject>();
 
-            for (int i = 0; i < 100; ++i) {
+            for (int i = 0; i < num; ++i) {
                 ChartDataObject obj = new ChartDataObject { Date = App.pp[i].DateTime, Value = App.pp[i].High };
                 data.Add(obj);
             }
@@ -116,32 +126,5 @@ namespace GDAX {
             return data;
         }
 
-        private List<PricePoint> LoadChartContents() {
-            List<PricePoint> lstSource = new List<PricePoint>();
-            lstSource.Add(new PricePoint() { Time = App.pp[0].Date.Substring(8, 5), Value = App.pp[0].High });
-            lstSource.Add(new PricePoint() { Time = App.pp[1].Date.Substring(8, 5), Value = App.pp[1].High });
-            lstSource.Add(new PricePoint() { Time = App.pp[2].Date.Substring(8, 5), Value = App.pp[2].High });
-            lstSource.Add(new PricePoint() { Time = App.pp[3].Date.Substring(8, 5), Value = App.pp[3].High });
-            lstSource.Add(new PricePoint() { Time = App.pp[4].Date.Substring(8, 5), Value = App.pp[4].High });
-            lstSource.Add(new PricePoint() { Time = App.pp[5].Date.Substring(8, 5), Value = App.pp[5].High });
-
-            return lstSource;
-
-        }
-
     }
 }
-/*
- *          List<ChartDataObject> dataSouce = new List<ChartDataObject>();
-
-            for (int i = 0; i < 100; ++i) {
-                ChartDataObject obj = new ChartDataObject { Date = App.pp[i].DateTime, Value = App.pp[i].High };
-                dataSouce.Add(obj);
-            }
-
-            Telerik.UI.Xaml.Controls.Chart.LineSeries series = (Telerik.UI.Xaml.Controls.Chart.LineSeries)LTC_Chart.Series[0];
-            series.CategoryBinding = new PropertyNameDataPointBinding() { PropertyName = "Date" };
-            series.ValueBinding = new PropertyNameDataPointBinding() { PropertyName = "Value" };
-
-            series.ItemsSource = dataSouce;
-*/
