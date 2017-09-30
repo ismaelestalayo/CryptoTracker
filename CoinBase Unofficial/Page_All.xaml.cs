a451c86833b3639c09e03c341fe8c811946853b8
Ismael <ismael.em@outlook.com>
2017-09-30 22:39:41 +0200

    updated screenshots

--- CoinBase Unofficial/Page_All.xaml.cs
+++ CoinBase Unofficial/Page_All.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Windows.UI.Xaml.Controls;
+using Telerik.UI.Xaml.Controls.Chart;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -8,57 +9,83 @@ namespace CoinBase {
 
         public Page_All() {
             this.InitializeComponent();
+
+            List<App.ChartDataObject> data = new List<App.ChartDataObject>();
+
+            for (int i = 0; i < App.ppBTC.Count - 1; i++) {
+                data.Add(new App.ChartDataObject() {
+                    Date = App.ppBTC[i].DateTime,
+                    Value = App.ppBTC[i].Low,
+                    Low  = App.ppBTC[i].Low,
+                    High = App.ppBTC[i].High
+                });
+            }
+
+            LineSeries BTCserie = (LineSeries)AllChartt.Series[0];
+            BTCserie.CategoryBinding = new PropertyNameDataPointBinding() { PropertyName = "Date" };
+            BTCserie.ValueBinding = new PropertyNameDataPointBinding() { PropertyName = "Value" };
+            BTCserie.ItemsSource = data;
+
+            data.Clear();
+            for (int i = 0; i < App.ppBTC.Count - 1; i++) {
+                data.Add(new App.ChartDataObject() {
+                    Date = App.ppETH[i].DateTime,
+                    Value  = App.ppETH[i].Low,
+                    Low  = App.ppETH[i].Low,
+                    High = App.ppETH[i].High
+                });
+            }
+
+            LineSeries ETHserie = (LineSeries)AllChartt.Series[1];
+            ETHserie.CategoryBinding = new PropertyNameDataPointBinding() { PropertyName = "Date" };
+            ETHserie.ValueBinding = new PropertyNameDataPointBinding() { PropertyName = "Value" };
+            ETHserie.ItemsSource = data;
+
+            data.Clear();
+            for (int i = 0; i < App.ppBTC.Count - 1; i++) {
+                data.Add(new App.ChartDataObject() {
+                    Date = App.ppLTC[i].DateTime,
+                    Value  = App.ppLTC[i].Low,
+                    Low  = App.ppLTC[i].Low,
+                    High = App.ppLTC[i].High
+                });
+            }
+
+            LineSeries LTCserie = (LineSeries)AllChartt.Series[2];
+            LTCserie.CategoryBinding = new PropertyNameDataPointBinding() { PropertyName = "Date" };
+            LTCserie.ValueBinding = new PropertyNameDataPointBinding() { PropertyName = "Value" };
+            LTCserie.ItemsSource = data;
+
+
         }
 
         private void Chart_BTC(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
             CheckBox c = sender as CheckBox;
             if (c.IsChecked == true) {
+                AllChartt.Series[0].Visibility = Windows.UI.Xaml.Visibility.Visible;
 
-                List<App.ChartDataObject> data = new List<App.ChartDataObject>();
-                for (int i = 0; i < 24; i++) {
-                    data.Add(new App.ChartDataObject() {
-                        Date = App.ppBTC[i].DateTime,
-                        Low  = App.ppBTC[i].Low,
-                        High = App.ppBTC[i].High
-                    });
-                }
-
-                AllChart.DataContext = data;
+            } else {
+                AllChartt.Series[0].Visibility = Windows.UI.Xaml.Visibility.Collapsed;
             }
         }
 
         private void Chart_ETH(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
             CheckBox c = sender as CheckBox;
             if (c.IsChecked == true) {
-                
+                AllChartt.Series[1].Visibility = Windows.UI.Xaml.Visibility.Visible;
 
-                List<App.ChartDataObject> data = new List<App.ChartDataObject>();
-                for (int i = 0; i < 24; i++) {
-                    data.Add(new App.ChartDataObject() {
-                        Date = App.ppETH[i].DateTime,
-                        Low  = App.ppETH[i].Low,
-                        High = App.ppETH[i].High
-                    });
-                }
-
-                AllChart.DataContext = data;
+            } else {
+                AllChartt.Series[1].Visibility = Windows.UI.Xaml.Visibility.Collapsed;
             }
         }
 
         private void Chart_LTC(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
             CheckBox c = sender as CheckBox;
             if (c.IsChecked == true) {
+                AllChartt.Series[2].Visibility = Windows.UI.Xaml.Visibility.Visible;
 
-                List<App.ChartDataObject> data = new List<App.ChartDataObject>();
-                for (int i = 0; i < 24; i++) {
-                    data.Add(new App.ChartDataObject() {
-                        Date = App.ppLTC[i].DateTime,
-                        Low  = App.ppLTC[i].Low,
-                        High = App.ppLTC[i].High
-                    });
-                }
-
-                AllChart.DataContext = data;
+            } else {
+                AllChartt.Series[2].Visibility = Windows.UI.Xaml.Visibility.Collapsed;
             }
         }
     }
