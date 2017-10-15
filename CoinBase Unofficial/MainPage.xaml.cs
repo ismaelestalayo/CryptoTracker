7db408eeba5fdd87dd986532fccb2e9efadf75dd
Ismael <ismael.em@outlook.com>
2017-10-15 22:32:12 +0200

    Huge overhaul for 1.6

--- CoinBase Unofficial/MainPage.xaml.cs
+++ CoinBase Unofficial/MainPage.xaml.cs
@@ -1,20 +1,15 @@
-﻿using System;
+﻿using CoinBase.Helpers;
 using System.Threading.Tasks;
-using CoinBase.Helpers;
-using Microsoft.Toolkit.Uwp.Notifications;
-using Microsoft.Toolkit.Uwp.UI.Animations;
 using Windows.ApplicationModel.Core;
-using Windows.Data.Xml.Dom;
 using Windows.Graphics.Display;
 using Windows.UI;
-using Windows.UI.Notifications;
-using Windows.UI.Popups;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
-namespace CoinBase {
+namespace CoinBase
+{
     public sealed partial class MainPage : Page {
 
         private SolidColorBrush Color_CoinBase = new SolidColorBrush(Color.FromArgb(255, 0, 91, 148));
@@ -33,96 +28,70 @@ namespace CoinBase {
 
             /// Alpha channel does nothing 
             /// (guess it's not supported on TitleBars
-            titleBar.BackgroundColor = Color.FromArgb(255, 0, 91, 148);
-            titleBar.ForegroundColor = Color.FromArgb(255, 255, 255, 255);
-            titleBar.ButtonBackgroundColor = Color.FromArgb(255, 34, 34, 34);  //New minimal gray
-            titleBar.ButtonForegroundColor = Color.FromArgb(255, 255, 255, 255);
+            titleBar.BackgroundColor = Color.FromArgb(0, 0, 91, 148);
+            titleBar.ForegroundColor = Color.FromArgb(255, 0, 0, 0);
+            titleBar.ButtonBackgroundColor = Color.FromArgb(0, 34, 34, 34);  //New minimal gray
+            titleBar.ButtonForegroundColor = Color.FromArgb(255, 0, 0, 0);
 
-            titleBar.InactiveBackgroundColor = Color.FromArgb(255, 0, 91, 148);
-            titleBar.InactiveForegroundColor = Color.FromArgb(255, 255, 255, 255);
+            titleBar.InactiveBackgroundColor = Color.FromArgb(0, 0, 91, 148);
+            titleBar.InactiveForegroundColor = Color.FromArgb(255, 0, 0, 0);
 
             //titleBar.ButtonHoverBackgroundColor = Color.FromArgb(0, 20, 20, 20);
             //titleBar.ButtonPressedBackgroundColor = Color.FromArgb(0, 50, 50, 50);
-            titleBar.ButtonInactiveBackgroundColor = Color.FromArgb(0, 0, 91, 148);
+            titleBar.ButtonInactiveBackgroundColor = Color.FromArgb(0, 0, 0, 0);
             //titleBar.ButtonInactiveForegroundColor = Color.FromArgb(0, 255, 255, 255);
 
             if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar")) {
                 var statusBar = StatusBar.GetForCurrentView();
-                statusBar.BackgroundColor = Color.FromArgb(255, 34, 34, 34);
+                statusBar.BackgroundColor = Color.FromArgb(255, 242, 242, 242);
                 statusBar.BackgroundOpacity = 1;
                 DisplayInformation.AutoRotationPreferences = DisplayOrientations.Portrait;
             }
 
             FirstRunDialogHelper.ShowIfAppropriateAsync();
-            MainFrame.Navigate(typeof(Page_Home));
+
+            Frame0.Navigate(typeof(Page_Home));
+            Frame1.Navigate(typeof(Page_BTC));
+            Frame2.Navigate(typeof(Page_ETH));
+            Frame3.Navigate(typeof(Page_LTC));
+            FrameSettings.Navigate(typeof(Page_Settings));
             //SyncAll();
         }
 
-        private void MenuHome_Click(object sender, RoutedEventArgs e) {
-            MainFrame.Navigate(typeof(Page_Home));
-        }
-        private void MenuBTC_Click(object sender, RoutedEventArgs e) {
-            MainFrame.Navigate(typeof(Page_BTC));
-        }
-        public void MenuETH_Click(object sender, RoutedEventArgs e) {
-            MainFrame.Navigate(typeof(Page_ETH));
-        }
-        private void MenuLTC_Click(object sender, RoutedEventArgs e) {
-            MainFrame.Navigate(typeof(Page_LTC));
-        }
-        private void MenuPortfolio_Click(object sender, RoutedEventArgs e) {
-            MainFrame.Navigate(typeof(Page_Portfolio));
-        }
-        private void MenuSettings_Click(object sender, RoutedEventArgs e) {
-            MainFrame.Navigate(typeof(Page_Settings));
-        }
-
-        private void HamburgerLogo_Click(object sender, RoutedEventArgs e) {
-            //MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
-        }
         ////////////////////////////////////////////////////////////////////////////////////////
         private void UpdateButton_Click(object sender, RoutedEventArgs e) {
             SyncAll();
-            //if (r == 0) {
-            //    await UpdateIcon.Rotate(value: 360,
-            //                 centerX: 9,
-            //                 centerY: 9,
-            //                 duration: 1000, delay: 0).StartAsync();
-            //    r = 1;
-            //} else {
-            //    await UpdateIcon.Rotate(value: 0,
-            //                 centerX: 9,
-            //                 centerY: 9,
-            //                 duration: 1000, delay: 0).StartAsync();
-            //    r = 0;
-            //}
 
             LiveTile l = new LiveTile();
             l.UpdateLiveTile();
         }
 
+        private void SettingsButton_Click(object sender, RoutedEventArgs e){
+            
+        }
+
         private async Task SyncAll() {
-            string x = MainFrame.Content.ToString();
+            //string x = MainFrame.Content.ToString();
 
-            if (x.Equals("CoinBase.Page_Home")) {
-                var p = (Page_Home)MainFrame.Content;
-                p.UpdateHome();
+            //if (x.Equals("CoinBase.Page_Home")) {
+            //    var p = (Page_Home)MainFrame.Content;
+            //    p.UpdateHome();
 
-            } else if (x.Equals("CoinBase.Page_BTC")) {
-                var p = (Page_BTC)MainFrame.Content;
-                p.BTC_Update_click();
+            //} else if (x.Equals("CoinBase.Page_BTC")) {
+            //    var p = (Page_BTC)MainFrame.Content;
+            //    p.BTC_Update_click();
 
-            } else if (x.Equals("CoinBase.Page_ETH")) {
-                var p = (Page_ETH)MainFrame.Content;
-                p.ETH_Update_click();
+            //} else if (x.Equals("CoinBase.Page_ETH")) {
+            //    var p = (Page_ETH)MainFrame.Content;
+            //    p.ETH_Update_click();
 
-            } else if (x.Equals("CoinBase.Page_LTC")) {
-                var p = (Page_LTC)MainFrame.Content;
-                p.LTC_Update_click();
-            } else if (x.Equals("CoinBase.Page_Portfolio")) {
-                var p = (Page_Portfolio)MainFrame.Content;
-                p.UpdatePortfolio();
-            }
+            //} else if (x.Equals("CoinBase.Page_LTC")) {
+            //    var p = (Page_LTC)MainFrame.Content;
+            //    p.LTC_Update_click();
+            //} else if (x.Equals("CoinBase.Page_Portfolio")) {
+            //    var p = (Page_Portfolio)MainFrame.Content;
+            //    p.UpdatePortfolio();
+            //}
 
             LiveTile l = new LiveTile();
             l.UpdateLiveTile();
