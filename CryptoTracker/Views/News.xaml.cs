abb0b45ad64980b5a702e5c3bd3d009d67aec525
Ismael <ismael.em@outlook.com>
2019-03-18 21:29:02 +0100

    Animations on News

--- CryptoTracker/Views/News.xaml.cs
+++ CryptoTracker/Views/News.xaml.cs
@@ -7,6 +7,7 @@ using Windows.UI.Composition;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
 
 namespace CryptoTracker.Views {
 
@@ -78,14 +79,43 @@ namespace CryptoTracker.Views {
         }
 
         // ###############################################################################################
+        //  AdaptiveGridView Elements
         private void newsAdaptiveGridView_Click(object sender, ItemClickEventArgs e) {
 
             newsAdaptiveGridView.PrepareConnectedAnimation("toWebView", e.ClickedItem, "GridView_Element");
-
             this.Frame.Navigate(typeof(WebVieww), ((NewsItem)e.ClickedItem).url);
         }
 
-        
+        private void newsAdaptiveGridView_ContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args) {
+            var elementVisual = ElementCompositionPreview.GetElementVisual(args.ItemContainer);
+            if (args.InRecycleQueue) {
+                elementVisual.ImplicitAnimations = null;
+            } 
+            else {
+                if (elementVisual.ImplicitAnimations == null) {
+                    var compositor =
+                        ElementCompositionPreview.GetElementVisual(this).Compositor;
 
+                    var offsetAnimation = compositor.CreateVector3KeyFrameAnimation();
+                    offsetAnimation.Target = nameof(Visual.Offset);
+                    offsetAnimation.InsertExpressionKeyFrame(1.0f, "this.FinalValue");
+                    offsetAnimation.Duration = TimeSpan.FromMilliseconds(400);
+
+                    var rotationAnimation = compositor.CreateScalarKeyFrameAnimation();
+                    rotationAnimation.Target = nameof(Visual.RotationAngle);
+                    rotationAnimation.InsertKeyFrame(.5f, 0.160f);
+                    rotationAnimation.InsertKeyFrame(1f, 0f);
+                    rotationAnimation.Duration = TimeSpan.FromSeconds(400);
+
+                    var animationGroup = compositor.CreateAnimationGroup();
+                    animationGroup.Add(offsetAnimation);
+                    animationGroup.Add(rotationAnimation);
+
+                    elementVisual.ImplicitAnimations = compositor.CreateImplicitAnimationCollection();
+                    elementVisual.ImplicitAnimations[nameof(Visual.Offset)] = animationGroup;
+                }
+                elementVisual.ImplicitAnimations = elementVisual.ImplicitAnimations;
+            }
+        }
     }
 }
