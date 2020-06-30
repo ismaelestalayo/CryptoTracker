f8338aa1fefaab8e14033af15451ff46e0009cea
Ismael <ismael.em@outlook.com>
2020-06-30 15:41:50 +0200

    Added Privacy Policy

--- CryptoTracker/Views/FirstRunDialog.xaml.cs
+++ CryptoTracker/Views/FirstRunDialog.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Windows.ApplicationModel;
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -15,7 +19,10 @@ namespace CryptoTracker.Views {
             var package = Package.Current;
             var version = package.Id.Version;
 
-            Title.Text += string.Format(" {0}.{1}.{2}", version.Major, version.Minor, version.Revision);
+            Title.Text = string.Format("Welcome to Crypto Tracker {0}.{1}.{2}", version.Major, version.Minor, version.Revision);
+
+            logo.Source = (new UISettings().GetColorValue(UIColorType.Background) == Colors.Black) ?
+                new BitmapImage(new Uri("ms-appx:///Assets/CryptoTracker Square LightT.png")) : new BitmapImage(new Uri("ms-appx:///Assets/CryptoTracker Square DarkT.png"));
         }
         
     }
