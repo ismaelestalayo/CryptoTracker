fe8c754ae84f226516e41f21e3bc66562fd0940a
Ismael <ismael.em@outlook.com>
2020-07-13 18:58:52 +0200

    optimized News page

--- CryptoTracker/Views/News.xaml.cs
+++ CryptoTracker/Views/News.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using System.Linq;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -8,31 +9,50 @@ using Windows.UI.Xaml.Controls;
 
 namespace CryptoTracker.Views {
     // ###############################################################################################
+    public class SourceInfo {
+        public string name { get; set; }
+        public string lang { get; set; }
+        public string img { get; set; }
+    }
+
+    public class NewsData {
+        public string id { get; set; }
+        public string guid { get; set; }
+        public int published_on { get; set; }
+        public string imageurl { get; set; }
+        public string title { get; set; } = "null";
+        public string url { get; set; } = "null";
+        public string source { get; set; } = "null";
+        public string body { get; set; }
+        public string tags { get; set; }
+        public string categories { get; set; } = "null";
+        public List<string> categorylist { get; set; }
+        public string upvotes { get; set; }
+        public string downvotes { get; set; }
+        public string lang { get; set; }
+        public SourceInfo source_info { get; set; }
+    }
+
     // News item for the AdaptiveGridView
     public class NewsItem {
-        public string imageURL  { get; set; }
-        public string title     { get; set; }
-        public string url       { get; set; }
-        public string source    { get; set; }
-        public string body      { get; set; }
-        public string tags      { get; set; }
-        public string language  { get; set; }
+        public int Type { get; set; }
+        public string Message { get; set; }
+        public List<object> Promoted { get; set; }
+        public List<NewsData> Data { get; set; }
+        public bool HasWarning { get; set; }
     }
+
+
 
     // ###############################################################################################
     // ###############################################################################################
     // ###############################################################################################
     public sealed partial class News : Page {
 
-        private List<NewsItem> NewsTilesList { get; set; }
-
         public News() {
             this.InitializeComponent();
 
-            NewsTilesList = new List<NewsItem>();
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             getNewsAsync();
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         
@@ -45,31 +65,18 @@ namespace CryptoTracker.Views {
             HttpClient httpClient = new HttpClient();
             HttpResponseMessage httpResponse = new HttpResponseMessage();
 
-            var response = "";
-
             try {
                 httpResponse = await httpClient.GetAsync(uri);
                 httpResponse.EnsureSuccessStatusCode();
 
-                response = await httpResponse.Content.ReadAsStringAsync();
+                var response = await httpResponse.Content.ReadAsStringAsync();
                 var data = JToken.Parse(response);
 
-                int lastIndex = ((JContainer)data["Data"]).Count;
-                for (int i = 0; i < lastIndex; i++) {
-                    NewsTilesList.Add(new NewsItem {
-                        imageURL    = data["Data"][i]["imageurl"].ToString(),
-                        title       = data["Data"][i]["title"].ToString(),
-                        url         = data["Data"][i]["url"].ToString(),
-                        source      = data["Data"][i]["source"].ToString(),
-                        body        = data["Data"][i]["body"].ToString(),
-                        tags        = data["Data"][i]["tags"].ToString(),
-                        language    = data["Data"][i]["lang"].ToString(),
-                    });
-                    NewsAdaptiveGridView.ItemsSource = NewsTilesList;
-                    
+                var news = JToken.Parse(data.ToString()).ToObject<NewsItem>();
+                foreach (NewsData n in news.Data) {
+                    n.categorylist = n.categories.Split('|').ToList();
                 }
-                
-                //NewsAdaptiveGridView.ItemsSource = NewsTilesList;
+                NewsAdaptiveGridView.ItemsSource = news.Data;
 
             } catch (Exception ex) {
                 await new MessageDialog(ex.Message).ShowAsync();
@@ -85,7 +92,7 @@ namespace CryptoTracker.Views {
         private void NewsAdaptiveGridView_Click(object sender, ItemClickEventArgs e) {
 
             NewsAdaptiveGridView.PrepareConnectedAnimation("toWebView", e.ClickedItem, "GridView_Element");
-            this.Frame.Navigate(typeof(WebVieww), ((NewsItem)e.ClickedItem).url);
+            this.Frame.Navigate(typeof(WebVieww), ((NewsData)e.ClickedItem).url);
         }
     }
 }
