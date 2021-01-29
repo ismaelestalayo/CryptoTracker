7ed0e0b0a54181fb5e8d2c6cb40ba557b964ec56
Ismael <ismael.em@outlook.com>
2021-01-29 14:50:38 +0100

    CryptoCompare: GetHistoric

--- CryptoTracker/APIs/CryptoCompare.cs
+++ CryptoTracker/APIs/CryptoCompare.cs
@@ -1,7 +1,23 @@
-﻿using System;
+﻿using CryptoTracker.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace CryptoTracker.APIs {
 	class CryptoCompare {
+
+        public class CryptoCompareResponse {
+            public string Response { get; set; }
+            public int Type { get; set; }
+            public bool Aggregated { get; set; }
+            public bool FirstValueInArray { get; set; }
+            public double TimeTo { get; set; }
+            public double TimeFrom { get; set; }
+            public object ConversionType { get; set; }
+            public object Data { get; set; }
+        }
 
         /* ###############################################################################################
          * Gets the current price of a coin (in the currency set by App.coin)
@@ -37,5 +53,36 @@ namespace CryptoTracker.APIs {
             }
         }
 
+        /* ###############################################################################################
+         * Gets the current price of a coin (in the currency set by App.coin)
+         * 
+         * Arguments: 
+         * - crypto (BTC, ETH...)
+         * - market
+        */
+        internal static async void GetHistoric(string crypto, string time, int limit) {
+            var coin = App.coin;
+
+            //CCCAGG Bitstamp Bitfinex Coinbase HitBTC Kraken Poloniex 
+            string URL = string.Format("https://min-api.cryptocompare.com/data/histo{0}?e=CCCAGG&fsym={1}&tsym={2}&limit={3}", time, crypto, coin, limit);
+
+            if (limit == 0)
+                URL = string.Format("https://min-api.cryptocompare.com/data/histoday?e=CCCAGG&fsym={0}&tsym={1}&allData=true", crypto, coin);
+
+            Uri uri = new Uri(URL);
+
+            try {
+                var data = await App.GetStringAsync(uri);
+                //var data = JToken.Parse(response);
+                var z = new List<JSONhistoric>();
+                var response = JsonSerializer.Deserialize<CryptoCompareResponse>(data);
+
+                var r = JsonSerializer.Deserialize<object>(data);
+                var d = ((JsonElement)r).GetProperty("Data");
+            }
+            catch (Exception ex) {
+                var dontWait = ex;
+            }
+        }
     }
 }
