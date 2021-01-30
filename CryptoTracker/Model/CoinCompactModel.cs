c328f9f2623a1064c9110b95e81b53c346b0fdb6
Ismael <ismael.em@outlook.com>
2021-01-31 00:14:50 +0100

    adding MVVM

--- CryptoTracker/Model/CoinCompactModel.cs
+++ CryptoTracker/Model/CoinCompactModel.cs
@@ -1,22 +1,76 @@
 ﻿using CryptoTracker.Helpers;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
 using System.Collections.Generic;
 using Windows.UI;
 using Windows.UI.Xaml.Media;
 
 namespace CryptoTracker.Model {
-	internal class CoinCompactModel {
+	internal class CoinCompactModel : ObservableObject {
 		internal string Crypto { get; set; }
 		internal string Currency { get; set; }
-		internal double CurrentPrice { get; set; }
-		internal double CurrentDiff { get; set; }
-		internal string CurrentDiffArrow { get; set; }
-		internal Brush DiffFG { get; set; }
-		internal Brush ChartStroke { get; set; }
-		internal Color ChartFill1 { get; set; }
-		internal Color ChartFill2 { get; set; }
-		internal float HistoricMin { get; set; } = 20000;
-		internal float HistoricMax { get; set; } = 30000;
-		internal List<ChartData> HistoricValues { get; set; } = new List<ChartData>();
+
+		private double _currentPrice = 0;
+		internal double CurrentPrice {
+			get => _currentPrice;
+			set => SetProperty(ref _currentPrice, value);
+		}
+
+		private double _currentDiff = 0;
+		internal double CurrentDiff {
+			get => _currentDiff;
+			set => SetProperty(ref _currentDiff, value);
+		}
+
+		private string _currentDiffArrow = "▲";
+		internal string CurrentDiffArrow {
+			get => _currentDiffArrow;
+			set => SetProperty(ref _currentDiffArrow, value);
+		}
+
+		private Brush _diffFG = (SolidColorBrush)App.Current.Resources["pastelGreen"];
+		internal Brush DiffFG {
+			get => _diffFG;
+			set => SetProperty(ref _diffFG, value);
+		}
+
+		private Brush _chartStroke = (SolidColorBrush)App.Current.Resources["pastelGreen"];
+		internal Brush ChartStroke {
+			get => _chartStroke;
+			set => SetProperty(ref _chartStroke, value);
+		}
+
+
+		private Color _chartFill1 = ((SolidColorBrush)App.Current.Resources["pastelGreen"]).Color;
+		private Color _chartFill2 = ((SolidColorBrush)App.Current.Resources["pastelGreen"]).Color;
+
+		internal Color ChartFill1 {
+			get => _chartFill1;
+			set => SetProperty(ref _chartFill1, value);
+		}
+		internal Color ChartFill2 {
+			get => _chartFill2;
+			set => SetProperty(ref _chartFill2, value);
+
+		}
+
+		private float _historicMin = 20000;
+		internal float HistoricMin {
+			get => _historicMin;
+			set => SetProperty(ref _historicMin, value);
+		}
+
+		private float _historicMax = 20000;
+		internal float HistoricMax {
+			get => _historicMax;
+			set => SetProperty(ref _historicMax, value);
+		}
+
+		private List<ChartData> _historicValues = new List<ChartData>();
+		internal List<ChartData> HistoricValues {
+			get => _historicValues;
+			set => SetProperty(ref _historicValues, value);
+		}
+
 		internal string LogoSource { get; set; }
 	}
 }
