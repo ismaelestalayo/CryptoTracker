6607c15f0511d91072ab5ee8db3b383b14d07b06
Ismael <ismael.em@outlook.com>
2021-02-07 00:57:27 +0100

    CoinDetails: MVVM

---
+++ CryptoTracker/ViewModels/CoinDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CryptoTracker.Helpers;
+using CryptoTracker.Model;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace CryptoTracker.ViewModels {
+	class CoinDetailsViewModel : ObservableObject {
+		private CoinCard _coinCard = new CoinCard();
+
+		public CoinCard CoinCard {
+			get => _coinCard;
+			set => SetProperty(ref _coinCard, value);
+		}
+
+		private CoinData _coinInfo = new CoinData();
+
+		public CoinData CoinInfo {
+			get => _coinInfo;
+			set => SetProperty(ref _coinInfo, value);
+		}
+	}
+}
--- CryptoTracker/Views/CoinDetails.xaml.cs
+++ CryptoTracker/Views/CoinDetails.xaml.cs
@@ -1,16 +1,12 @@
 ﻿using CryptoTracker.APIs;
 using CryptoTracker.Helpers;
 using CryptoTracker.Views;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.Toolkit.Uwp.UI.Controls;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.Runtime.CompilerServices;
+using System.Linq;
 using System.Threading.Tasks;
-using Telerik.UI.Xaml.Controls.Chart;
 using Windows.System.Threading;
+using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Popups;
 using Windows.UI.ViewManagement;
@@ -18,47 +14,22 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 namespace CryptoTracker {
-
-    public class ChartLine {
-        public PropertyNameDataPointBinding Datee { get; set; }
-        public PropertyNameDataPointBinding Valuee { get; set; }
-    }
-
-    public class CoinDataWrapper : INotifyPropertyChanged {
-        private CoinData _cd;
-        public CoinData cd {
-            get { return _cd; }
-            set { _cd = value; OnPropertyChanged("cd"); }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string propertyName = null) {
-            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    public sealed partial class CoinDetails : Page {
-        private static int limit = 60;
-        private static string timeSpan = "week";
+	public sealed partial class CoinDetails : Page {
+        private static int limit = 168;
+        private static string timeSpan = "hour";
         private static ThreadPoolTimer PeriodicTimer;
 
-        internal string crypto { get; set; }
-        internal string currency = App.currency;
-        internal string currencySymbol = App.currencySymbol;
-        public CoinDataWrapper cdw { get; set; }
+        private Color ChartTrackBall = Color.FromArgb(255, 180, 50, 180);
+        private SolidColorBrush ChartTrackBallBrush = new SolidColorBrush(Color.FromArgb(255, 180, 50, 180));
 
         public CoinDetails() {
             this.InitializeComponent();
 
-            cdw = new CoinDataWrapper();
-
             if (ApplicationView.GetForCurrentView().IsViewModeSupported(ApplicationViewMode.CompactOverlay))
                 CompactOverlay_btn.Visibility = Visibility.Visible;
-            
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e) {
@@ -69,30 +40,16 @@ namespace CryptoTracker {
                 
                 
                 // Page title
-                crypto = e.Parameter?.ToString() ?? "NULL";
+                var crypto = e.Parameter?.ToString().ToUpperInvariant() ?? "NULL";
+                viewModel.CoinCard.Crypto = crypto;
 
-                var currentCoin = App.coinList.Find(x => x.symbol == crypto);
-                mainTitle.Text = string.Format("{0} ({1})", currentCoin.name, currentCoin.symbol);
+                var coin = App.coinList.Find(x => x.symbol == crypto);
+                viewModel.CoinCard.CryptoFullName = coin.name;
+                viewModel.CoinCard.CryptoSymbol = coin.symbol;
 
-                try {
-                    mainTitleLogo.Source = new BitmapImage(new Uri("ms-appx:///Assets/Icons/icon" + crypto.ToUpper(CultureInfo.InvariantCulture) + ".png"));
-                } catch(Exception) {
-                    mainTitleLogo.Source = new BitmapImage(new Uri("ms-appx:///Assets/Icons/iconNULL.png"));
-                    Analytics.TrackEvent(string.Format("Missing icon: {0}", crypto));
-                }
-                mainTitleLogo.Visibility = Visibility.Visible;
+                FavIcon.Content = App.pinnedCoins.Contains(crypto) ? "\uEB52" : "\uEB51";
 
-                try {
-                    ((SolidColorBrush)App.Current.Resources["coinColor"]).Color  = ((SolidColorBrush)App.Current.Resources[crypto.ToUpper(CultureInfo.InvariantCulture) + "_color"]).Color;
-                    ((SolidColorBrush)App.Current.Resources["coinColorT"]).Color = ((SolidColorBrush)App.Current.Resources[crypto.ToUpper(CultureInfo.InvariantCulture) + "_colorT"]).Color;
-                } catch {
-                    ((SolidColorBrush)App.Current.Resources["coinColor"]).Color  = ((SolidColorBrush)App.Current.Resources["Main_WhiteBlack"]).Color;
-                    ((SolidColorBrush)App.Current.Resources["coinColorT"]).Color = ((SolidColorBrush)App.Current.Resources["Main_WhiteBlackT"]).Color;
-                }
-
-                FavIcon.Content = App.pinnedCoins.Contains(crypto.ToUpper(CultureInfo.InvariantCulture)) ? "\uEB52" : "\uEB51";
-
-                InitValues();
+                InitValues(coin);
             }
             catch (Exception ex){
                 var message = "There was an error loading that coin. Try again later.";
@@ -105,10 +62,9 @@ namespace CryptoTracker {
                 PeriodicTimer.Cancel();
         }
 
-        private async void InitValues() {
+        private async void InitValues(CoinBasicInfo coin) {
 
-            CoinBasicInfo coin = App.coinList.Find(x => x.symbol == crypto);
-            cdw.cd = await API_CoinGecko.GetCoin(coin.name);
+            viewModel.CoinInfo = await API_CoinGecko.GetCoin(coin.name);
 
             TimeSpan period = TimeSpan.FromSeconds(30);
             PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (source) => {
@@ -124,94 +80,56 @@ namespace CryptoTracker {
                 TimerangeButton_Click(r, null);
 
             } catch (Exception) {
-                LoadingControl.IsLoading = false;
+                viewModel.CoinCard.IsLoading = false;
                 //var dontWait = new MessageDialog(ex.ToString()).ShowAsync();
             }
         }
 
-        // #########################################################################################
-        // #########################################################################################
-        // #########################################################################################
+        /// #########################################################################################
+        /// #########################################################################################
+        /// #########################################################################################
         internal async void UpdatePage() {
-            if (LoadingControl == null)
-                LoadingControl = new Loading();
-
-            LoadingControl.IsLoading = true;
+            viewModel.CoinCard.IsLoading = true;
 
             await UpdateCoin();
-            verticalAxis.Minimum = GraphHelper.GetMinimum(App.historic);
-            verticalAxis.Maximum = GraphHelper.GetMaximum(App.historic);
-            dateTimeAxis = App.AdjustAxis(dateTimeAxis, timeSpan);
             await Get24Volume();
             //CryptoCompare.GetExchanges(crypto);
         }
 
-        // #########################################################################################
+        /// #########################################################################################
         private async Task UpdateCoin() {
+            var crypto = viewModel.CoinCard.Crypto;
+            viewModel.CoinCard.Price = await CryptoCompare.GetPriceAsync(crypto);
 
-            var price = await CryptoCompare.GetPriceAsync(crypto);
-            mainTitleVal.Text = price.ToString() + App.currencySymbol;
-            mainTitleVal.Visibility  = Visibility.Visible;
-            mainTitleDiff.Visibility = Visibility.Visible;
-
-            switch (timeSpan) {
-                case "hour":
-                case "day":
-                    await App.GetHisto(crypto, "minute", limit);
-                    break;
-
-                case "week":
-                case "month":
-                    await App.GetHisto(crypto, "hour", limit);
-                    break;
-
-                case "year":
-                    await App.GetHisto(crypto, "day", limit);
-                    break;
-
-                case "all":
-                    await App.GetHisto(crypto, "day", 0);
-                    break;
+            /// Get Historic and create List of ChartData for the chart
+            var histo = await CryptoCompare.GetHistoricAsync(crypto, timeSpan, limit);
+            var chartData = new List<ChartData>();
+            foreach (var h in histo) {
+                chartData.Add(new ChartData() {
+                    Color = viewModel.CoinCard.ChartStroke.Color,
+                    Date = h.DateTime,
+                    Value = h.Average,
+                    Volume = h.volumefrom
+                });
             }
+            viewModel.CoinCard.ChartData = chartData;
 
-            List<ChartData> data = new List<ChartData>();
-            for (int i = 0; i < App.historic.Count; ++i) {
-                ChartData obj = new ChartData {
-                    Date   =  App.historic[i].DateTime,
-                    Value  = (App.historic[i].Low + App.historic[i].High) / 2,
-                    Low    =  App.historic[i].Low,
-                    High   =  App.historic[i].High,
-                    Open   =  App.historic[i].Open,
-                    Close  =  App.historic[i].Close,
-                    Volume =  App.historic[i].Volumefrom
-                };
-                data.Add(obj);
-            }
+            /// Calculate min-max to adjust axis
+            var MinMax = GraphHelper.GetMinMaxOfArray(chartData.Select(d => d.Value).ToList());
+            viewModel.CoinCard.PricesMinMax = MinMax;
 
-            float oldestPrice = App.historic[0].Close;
-            float newestPrice = App.historic[App.historic.Count - 1].Close;
-            float d = (float)Math.Round((newestPrice / oldestPrice - 1) * 100, 2);
+            /// Calculate the price difference
+            double oldestPrice = histo[0].Average;
+            double newestPrice = histo[histo.Count - 1].Average;
+            double diff = (double)Math.Round((newestPrice / oldestPrice - 1) * 100, 2);
+            viewModel.CoinCard.Diff = diff;
 
-            if (d < 0) {
-                mainTitleDiff.Foreground = (SolidColorBrush)Application.Current.Resources["pastelRed"];
-                d = Math.Abs(d);
-                mainTitleDiff.Text = "▼" + d.ToString() + "%";
-            } else {
-                mainTitleDiff.Foreground = (SolidColorBrush)Application.Current.Resources["pastelGreen"];
-                mainTitleDiff.Text = "▲" + d.ToString() + "%";
-            }
-
-            SplineAreaSeries series = (SplineAreaSeries)PriceChart.Series[0];
-            series.CategoryBinding = new PropertyNameDataPointBinding() { PropertyName = "Date" };
-            series.ValueBinding = new PropertyNameDataPointBinding() { PropertyName = "Value" };
-            series.ItemsSource = data;
-
-            if (LoadingControl != null)
-                LoadingControl.IsLoading = false;
+            viewModel.CoinCard.IsLoading = false;
         }
 
         
         async private Task Get24Volume() {
+            var crypto = viewModel.CoinCard.Crypto;
             await App.GetHisto(crypto, "hour", 24);
 
             List<ChartData> data = new List<ChartData>();
@@ -226,36 +144,37 @@ namespace CryptoTracker {
 
         // #########################################################################################
         private void TimerangeButton_Click(object sender, RoutedEventArgs e) {
-            LoadingControl.IsLoading = true;
+            viewModel.CoinCard.IsLoading = true;
+
             RadioButton btn = sender as RadioButton;
 
             switch (btn.Content) {
                 case "hour":
-                    timeSpan = "hour";
+                    timeSpan = "minute";
                     limit = 60;
                     break;
 
                 case "day":
-                    timeSpan = "day";
+                    timeSpan = "minute";
                     limit = 1500;
                     break;
 
                 case "week":
-                    timeSpan = "week";
+                    timeSpan = "hour";
                     limit = 168;
                     break;
 
                 case "month":
-                    timeSpan = "month";
+                    timeSpan = "hour";
                     limit = 744;
                     break;
                 case "year":
-                    timeSpan = "year";
+                    timeSpan = "day";
                     limit = 365;
                     break;
 
                 case "all":
-                    timeSpan = "all";
+                    timeSpan = "day";
                     limit = 0;
                     break;
 
@@ -264,22 +183,23 @@ namespace CryptoTracker {
         }
 
         private void PinCoin_btn(object sender, RoutedEventArgs e) {
-            var c = crypto;
-            if (!App.pinnedCoins.Contains(c)) {
-                App.pinnedCoins.Add(c);
-                //Home.AddCoinHome(c);
+            var crypto = viewModel.CoinCard.Crypto;
+            if (!App.pinnedCoins.Contains(crypto)) {
+                App.pinnedCoins.Add(crypto);
+                //Home.AddCoinHome(crypto);
                 FavIcon.Content = "\uEB52";
-                inAppNotification.Show(c + " pinned to home.", 2000);
+                inAppNotification.Show(crypto + " pinned to home.", 2000);
             }
             else {
-                //Home.RemoveCoinHome(c);
-                App.pinnedCoins.Remove(c);
+                //Home.RemoveCoinHome(crypto);
+                App.pinnedCoins.Remove(crypto);
                 FavIcon.Content = "\uEB51";
-                inAppNotification.Show(c + " unpinned from home.", 2000);
+                inAppNotification.Show(crypto + " unpinned from home.", 2000);
             }
         }
 
 		private async void CompactOverlay_btn_click(object sender, RoutedEventArgs e) {
+            var crypto = viewModel.CoinCard.Crypto;
             var view = ApplicationView.GetForCurrentView();
 
 			var preferences = ViewModePreferences.CreateDefault(ApplicationViewMode.CompactOverlay);
