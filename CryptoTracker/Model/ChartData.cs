fad93dff79624ea6b1a284a62b38ea6b5474e6bc
Ismael <ismael.em@outlook.com>
2021-02-07 15:21:49 +0100

    ChartData: moved to Model\

--- CryptoTracker/Model/ChartData.cs
+++ CryptoTracker/Model/ChartData.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using Windows.UI;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 
 namespace CryptoTracker.Helpers {
     // #########################################################################################
-    //  Object for plotting charts
-    public class ChartData {
+	//  Object for plotting charts
+	public class ChartData {
         public DateTime Date { get; set; }
         public float Value { get; set; }
         public float Low { get; set; }
@@ -16,5 +15,6 @@ namespace CryptoTracker.Helpers {
         public float Volume { get; set; }
         public string Category { get; set; }
         public Color Color { get; set; } = Color.FromArgb(255, 128, 128, 128);
+        public SolidColorBrush Brush { get; set; } = new SolidColorBrush(Color.FromArgb(255, 128, 128, 128));
     }
 }
