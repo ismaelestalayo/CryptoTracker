b7385c2097f54ac0c99cbb7dc63028fe4379930f
Ismael <ismael.em@outlook.com>
2021-02-09 19:48:24 +0100

    Added NumbersHelper

---
+++ CryptoTracker/Helpers/NumberHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace CryptoTracker.Helpers {
+	class NumberHelper {
+        public static string AddUnitPrefix(double num) {
+            if (num > 999999999) {
+                return num.ToString("0,,,.##B", CultureInfo.InvariantCulture);
+            }
+            else
+            if (num > 999999) {
+                return num.ToString("0,,.##M", CultureInfo.InvariantCulture);
+            }
+            else
+            if (num > 999) {
+                num = Math.Round(num, 2);
+                return num.ToString(CultureInfo.InvariantCulture);
+            }
+            else {
+                num = Math.Round(num, 3);
+                return num.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+
+        public static double Rounder(double price) {
+            if (price > 99)
+                return Math.Round(price, 2);
+            else if (price > 10)
+                return Math.Round(price, 4);
+            else
+                return Math.Round(price, 6);
+		}
+    }
+}
