d3b2a12fd59df9a05bf5400f487c99409b2c9e3c
Ismael <ismael.em@outlook.com>
2021-02-13 19:52:29 +0100

    removed unnecesary imports

--- CryptoTracker/UserControls/ChartAreaFull.xaml.cs
+++ CryptoTracker/UserControls/ChartAreaFull.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using CryptoTracker.Models;
-using Telerik.UI.Xaml.Controls.Chart;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Shapes;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
