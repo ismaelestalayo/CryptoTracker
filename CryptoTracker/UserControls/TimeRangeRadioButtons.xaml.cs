8bb9b3f9859a12414baa6a59d7a9bf1ced3215cf
Ismael <ismael.em@outlook.com>
2021-02-14 17:09:36 +0100

    TimeRangeRadioButtons: new UserControl

---
+++ CryptoTracker/UserControls/TimeRangeRadioButtons.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CryptoTracker.UserControls {
+	public sealed partial class TimeRangeRadioButtons : UserControl {
+		public TimeRangeRadioButtons() {
+			this.InitializeComponent();
+		}
+
+		private void UpdateTimeSpan(object sender, RoutedEventArgs e) {
+			timeSpan = ((ContentControl)sender).Content.ToString();
+		}
+
+		private string timeSpan = "1w";
+		public string TimeSpan {
+			get => timeSpan;
+		}
+	}
+}
