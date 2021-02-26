f6a9bf2d9b7794c30129e1e493d3e1445c44e4ec
Ismael <ismael.em@outlook.com>
2021-02-26 23:39:18 +0100

    minor

--- CryptoTracker/UserControls/ChartArea.xaml.cs
+++ CryptoTracker/UserControls/ChartArea.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using CryptoTracker.Models;
-using Telerik.UI.Xaml.Controls.Chart;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Shapes;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace CryptoTracker.UserControls {
-	public sealed partial class ChartArea : UserControl {
+    public sealed partial class ChartArea : UserControl {
         public ChartArea() {
             this.InitializeComponent();
         }
