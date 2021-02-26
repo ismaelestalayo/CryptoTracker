1e07e7612efa26a9fc70300d82e591c9c9eb9550
Ismael <ismael.em@outlook.com>
2021-02-26 23:39:55 +0100

    Notification UserControl

---
+++ CryptoTracker/UserControls/Notification.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CryptoTracker.UserControls {
+    public sealed partial class Notification : UserControl {
+        public Notification() {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(string),
+            null);
+
+        public static readonly DependencyProperty MessageProperty =
+        DependencyProperty.Register(
+            nameof(Message),
+            typeof(string),
+            typeof(string),
+            null);
+
+        public static readonly DependencyProperty IsOpenProperty =
+        DependencyProperty.Register(
+            nameof(IsOpen),
+            typeof(bool),
+            typeof(bool),
+            null);
+
+        public string Title {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public string Message {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+
+        public bool IsOpen {
+            get => (bool)GetValue(IsOpenProperty);
+            set => SetValue(IsOpenProperty, value);
+        }
+    }
+}
