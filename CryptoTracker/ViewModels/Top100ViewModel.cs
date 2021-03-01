9f8fef68c09d325176c034c25d5bb28bb9aa5dec
Ismael <ismael.em@outlook.com>
2021-03-02 00:20:22 +0100

    Top100: use new Notifications

--- CryptoTracker/ViewModels/Top100ViewModel.cs
+++ CryptoTracker/ViewModels/Top100ViewModel.cs
@@ -1,20 +1,27 @@
 ﻿using CryptoTracker.Models;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using System;
 using System.Collections.Generic;
 
 namespace CryptoTracker.ViewModels {
-	public class Top100ViewModel : ObservableObject {
+    public class Top100ViewModel : ObservableRecipient {
 
-		private GlobalStats _globalStats;
-		public GlobalStats GlobalStats {
-			get => _globalStats;
-			set => SetProperty(ref _globalStats, value);
-		}
+        private GlobalStats _globalStats;
+        public GlobalStats GlobalStats {
+            get => _globalStats;
+            set => SetProperty(ref _globalStats, value);
+        }
 
-		private List<Top100card> _top100cards;
-		public List<Top100card> Top100cards {
-			get => _top100cards;
-			set => SetProperty(ref _top100cards, value);
-		}
-	}
+        private List<Top100card> _top100cards;
+        public List<Top100card> Top100cards {
+            get => _top100cards;
+            set => SetProperty(ref _top100cards, value);
+        }
+
+        public void InAppNotification(string title, string message = "", bool temporary = true) {
+            var tuple = new Tuple<string, string, bool>(title, message, temporary);
+            Messenger.Send(new NotificationMessage(tuple));
+        }
+    }
 }
--- CryptoTracker/Views/Top100.xaml.cs
+++ CryptoTracker/Views/Top100.xaml.cs
@@ -5,7 +5,7 @@ using Windows.UI.Xaml.Controls;
 
 namespace CryptoTracker.Views {
 
-	public sealed partial class Top100 : Page {
+    public sealed partial class Top100 : Page {
 
         public Top100() {
             this.InitializeComponent();
@@ -18,16 +18,16 @@ namespace CryptoTracker.Views {
 
             var topCoins = await CryptoCompare.GetTop100();
             vm.Top100cards = topCoins;
-			//topCoins.Sort((x, y) => y.MarketCapRaw.CompareTo(x.MarketCapRaw));
+            //topCoins.Sort((x, y) => y.MarketCapRaw.CompareTo(x.MarketCapRaw));
         }
 
         // #########################################################################################
         private void ListView_Click(object sender, ItemClickEventArgs e) {
             top100ListView.PrepareConnectedAnimation("toCoinDetails", e.ClickedItem, "listView_Element");
 
-			var crypto = ((Top100card)e.ClickedItem).CoinInfo.Name;
-			this.Frame.Navigate(typeof(CoinDetails), crypto);
-		}
+            var crypto = ((Top100card)e.ClickedItem).CoinInfo.Name;
+            this.Frame.Navigate(typeof(CoinDetails), crypto);
+        }
 
         private void Pin_click(object sender, RoutedEventArgs e) {
             //string c = ((Top100coin)((FrameworkElement)((FrameworkElement)sender).Parent).DataContext).Symbol;
@@ -35,10 +35,10 @@ namespace CryptoTracker.Views {
             var c = card.CoinInfo.Name;
             if (!App.pinnedCoins.Contains(c)) {
                 App.pinnedCoins.Add(c);
-                inAppNotification.Show(c + " pinned to home.", 2000);
+                vm.InAppNotification($"{c} pinned to Home.");
             } else {
                 App.pinnedCoins.Remove(c);
-                inAppNotification.Show(c + " unpinned from home.", 2000);
+                vm.InAppNotification($"{c} unpinned from Home.");
             }
             card.CoinInfo.FavIcon = card.CoinInfo.FavIcon.Equals("\uEB51") ? "\uEB52" : "\uEB51";
 
