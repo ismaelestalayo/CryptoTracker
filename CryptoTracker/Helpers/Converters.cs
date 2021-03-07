de8a10492b70749707ce7ca5624f20be16d3d6e0
Ismael <ismael.em@outlook.com>
2021-03-07 18:52:30 +0100

    Converters improvements

--- CryptoTracker/Helpers/Converters.cs
+++ CryptoTracker/Helpers/Converters.cs
@@ -4,57 +4,73 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
 namespace CryptoTracker.Helpers {
-    static class Converters {
-        public static string ShortenDateTime(DateTime date) => date.ToShortDateString();
-    }
-
+    /// Numbers
     public class GeneralNumberConverter : IValueConverter {
         public object Convert(object val, Type targetType, object param, string lang)
             => ((double)val).ToString("N", App.UserCulture);
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    /// Dates
+    public class DateTimeFormatter : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((DateTime)val).ToString(App.UserCulture);
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class DateTimeShortener : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((DateTime)val).ToShortDateString();
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class DateTimeOffsetShortener : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((DateTimeOffset)val).DateTime.ToShortDateString();
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    /// Others
+    public class VisibilityInverter : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)  => (Visibility)val == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        public object ConvertBack(object val, Type targetType, object param, string lang) => throw new NotImplementedException();
+    }
+
+    public class ListCountToVisibilityInvertedConverter : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((int)val) == 0 ? Visibility.Visible : Visibility.Collapsed;
 
         public object ConvertBack(object val, Type targetType, object param, string lang)
             => throw new NotImplementedException();
     }
 
-    public class VisibilityInverter : IValueConverter {
-        public object Convert(object value, Type targetType, object param, string language)  => (Visibility)value == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
-        public object ConvertBack(object value, Type targetType, object param, string language) => throw new NotImplementedException();
-    }
-
-    public class ListCountToVisibilityInvertedConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object param, string language) {
-            return ((int)value) == 0 ? Visibility.Visible : Visibility.Collapsed;
-        }
-
-        public object ConvertBack(object value, Type targetType, object param, string language) => throw new NotImplementedException();
-    }
     public class ListCountToVisibilityConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object param, string language) {
-            return ((int)value) == 0 ? Visibility.Collapsed : Visibility.Visible;
-        }
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((int)val) == 0 ? Visibility.Collapsed : Visibility.Visible;
 
-        public object ConvertBack(object value, Type targetType, object param, string language) => throw new NotImplementedException();
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
     }
 
     public class StringFormatConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object param, string language) {
+        public object Convert(object val, Type targetType, object param, string lang) {
             try {
-                var str = value.ToString();
+                var str = val.ToString();
                 var num = double.Parse(str);
                 if (!string.IsNullOrEmpty(str))
                     return num.ToString("N0", CultureInfo.CurrentCulture.NumberFormat);
-                return value;
+                return val;
             }
             catch {
-                return value;
+                return val;
             }
         }
 
-        public object ConvertBack(object value, Type targetType, object param, string language) => throw new NotImplementedException();
-    }
-
-    public class DateTimeShortener : IValueConverter {
-        public object Convert(object value, Type targetType, object param, string language) => ((DateTimeOffset)value).DateTime.ToShortDateString();
-        public object ConvertBack(object value, Type targetType, object param, string language) => throw new NotImplementedException();
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
     }
 }
