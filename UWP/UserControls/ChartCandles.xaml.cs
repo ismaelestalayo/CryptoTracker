cccfd887dcefd9d32eacaac334bd087eeb954db6
Ismael <ismael.em@outlook.com>
2021-04-06 17:04:43 +0200

    ChartCandles added

---
+++ UWP/UserControls/ChartCandles.xaml.cs
@@ -0,0 +1,26 @@
+﻿using UWP.Models;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UWP.UserControls {
+    public sealed partial class ChartCandles : UserControl {
+
+        public ChartCandles() {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ChartModelProperty =
+        DependencyProperty.Register(
+            nameof(ChartModel),
+            typeof(ChartModel),
+            typeof(ChartAreaFull),
+            null);
+
+        public ChartModel ChartModel {
+            get => (ChartModel)GetValue(ChartModelProperty);
+            set => SetValue(ChartModelProperty, value);
+        }
+    }
+}
