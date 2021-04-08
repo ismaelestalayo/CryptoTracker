c453142141423be0e74eed91fb1a7cc03e0c6f8a
Ismael <ismael.em@outlook.com>
2021-04-08 18:15:41 +0200

    CoinDetails + ChartAreaFull: QoL

--- UWP/Helpers/Converters.cs
+++ UWP/Helpers/Converters.cs
@@ -21,12 +21,12 @@ namespace UWP.Helpers {
 
     /// Bool
     public class BoolToVisibility : IValueConverter {
-    public object Convert(object val, Type targetType, object param, string lang)
-        => ((bool)val) ? Visibility.Visible : Visibility.Collapsed;
-    public object ConvertBack(object val, Type targetType, object param, string lang)
-        => throw new NotImplementedException();
-}
-public class BoolInverseToVisibility : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((bool)val) ? Visibility.Visible : Visibility.Collapsed;
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+    public class BoolInverseToVisibility : IValueConverter {
         public object Convert(object val, Type targetType, object param, string lang)
             => ((bool)val) ? Visibility.Collapsed : Visibility.Visible;
         public object ConvertBack(object val, Type targetType, object param, string lang)
