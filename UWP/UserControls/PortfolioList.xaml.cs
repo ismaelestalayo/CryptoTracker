77442d6d6fa3105e8ab1fed5166bc15221842d4b
Ismael <ismael.em@outlook.com>
2021-04-14 00:30:37 +0200

    PortfolioList: alpha

---
+++ UWP/UserControls/PortfolioList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using UWP.Models;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UWP.UserControls {
+    public sealed partial class PortfolioList : UserControl {
+        public PortfolioList() {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty PurchasesProperty =
+        DependencyProperty.Register(
+            nameof(Purchases),
+            typeof(ObservableCollection<PurchaseModel>),
+            typeof(PortfolioList),
+            null);
+
+        public ObservableCollection<PurchaseModel> Purchases {
+            get => (ObservableCollection<PurchaseModel>)GetValue(PurchasesProperty);
+            set => SetValue(PurchasesProperty, value);
+        }
+    }
+}
