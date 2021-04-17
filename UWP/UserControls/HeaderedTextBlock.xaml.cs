17777eded711529621730746893873a19a0f2a62
Ismael <ismael.em@outlook.com>
2021-04-18 01:43:02 +0200

    CoinDetails: HeaderedTextBlock

---
+++ UWP/UserControls/HeaderedTextBlock.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UWP.UserControls {
+    public sealed partial class HeaderedTextBlock : UserControl {
+        public HeaderedTextBlock() {
+            InitializeComponent();
+        }
+
+        private object topTextBlock;
+        public object TopTextBlock {
+            get => topTextBlock;
+            set => topTextBlock = value;
+        }
+
+        private TextBlock bottomTextBlock;
+        public TextBlock BottomTextBlock {
+            get => bottomTextBlock;
+            set => bottomTextBlock = value;
+        }
+
+        public HorizontalAlignment Alignment { get; set; } = HorizontalAlignment.Left;
+    }
+}
