4535db5fa89e8ffbcd5e3b76c86baa4c22a91f0b
Ismael <ismael.em@outlook.com>
2021-04-20 17:33:01 +0200

    Converters: reorg

---
+++ UWP/Converters/NumberConverters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UWP.Helpers;
+using Windows.UI.Xaml.Data;
+
+namespace UWP.Converters {
+    public class GeneralNumberConverter : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((double)val).ToString("N5", App.UserCulture).TrimEnd('0').Trim(',');
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class NumberRounder : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => NumberHelper.Rounder((double)val).ToString("N5", App.UserCulture).TrimEnd('0').Trim(',');
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class NumberUnitSuffixer : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => NumberHelper.AddUnitPrefix((double)val);
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class NumberSignPrefixer : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang) {
+            var z = new NumberRounder();
+            double num = double.Parse(z.Convert(val, targetType, param, lang).ToString(), App.UserCulture);
+            return string.Format("{0:+0.#####;-0.#####;}", (double)num);
+        }
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+}
---
+++ UWP/Converters/OtherConverters.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using UWP.Shared.Constants;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UWP.Converters {
+    public class NumberForeground : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => (double)val >= 0 ? ColorConstants.GetBrush("pastel_green") : ColorConstants.GetBrush("pastel_red");
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class VisibilityInverter : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => (Visibility)val == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class ListCountToVisibilityInvertedConverter : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((int)val) == 0 ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class ListCountToVisibilityConverter : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang)
+            => ((int)val) == 0 ? Visibility.Collapsed : Visibility.Visible;
+
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+
+    public class StringFormatConverter : IValueConverter {
+        public object Convert(object val, Type targetType, object param, string lang) {
+            try {
+                var str = val.ToString();
+                var num = double.Parse(str);
+                if (!string.IsNullOrEmpty(str))
+                    return num.ToString("N0", CultureInfo.CurrentCulture.NumberFormat);
+                return val;
+            }
+            catch {
+                return val;
+            }
+        }
+
+        public object ConvertBack(object val, Type targetType, object param, string lang)
+            => throw new NotImplementedException();
+    }
+}
