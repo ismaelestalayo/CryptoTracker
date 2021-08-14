a4324f36a730905367fba32ec57c3d41e13ca9b3
Ismael <ismael.em@outlook.com>
2021-08-14 17:37:35 +0200

    PortfolioEntryDialog better Tab support

--- UWP/Dialogs/PortfolioEntryDialog.xaml.cs
+++ UWP/Dialogs/PortfolioEntryDialog.xaml.cs
@@ -6,6 +6,7 @@ using UWP.Models;
 using UWP.Services;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 
 // The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -32,8 +33,8 @@ namespace CryptoTracker.Dialogs {
         private SuggestionCoin SuggestionCoin {
             get => suggestionCoin;
             set {
-                NewPurchase.Crypto = value.Symbol;
-                NewPurchase.CryptoName = value.Name;
+                NewPurchase.Crypto = value?.Symbol;
+                NewPurchase.CryptoName = value?.Name;
             }
         }
 
@@ -52,6 +53,10 @@ namespace CryptoTracker.Dialogs {
                 //}
             }
         }
+
+        private void CoinAutoSuggestBox_LostFocus(object sender, RoutedEventArgs e)
+            => AmountNumberBox.Focus(FocusState.Programmatic);
+
 
         private async void DialogBtn_LostFocus(object sender, RoutedEventArgs e) {
             // If we change the crypto, set the current price to 0 so everything updates
