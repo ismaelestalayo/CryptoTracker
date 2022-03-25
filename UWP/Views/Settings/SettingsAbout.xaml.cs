9e6c695fed3811ad38200bfd658e1aec09302c52
Ismael <ismael.em@outlook.com>
2022-03-26 00:55:40 +0100

    Settings NavView

--- UWP/Views/Settings/SettingsAbout.xaml.cs
+++ UWP/Views/Settings/SettingsAbout.xaml.cs
@@ -1,9 +1,11 @@
-﻿using Windows.ApplicationModel;
+﻿using UWP.Core.Constants;
+using Windows.ApplicationModel;
 using Windows.UI.Xaml.Controls;
 
 namespace UWP.Views {
     public sealed partial class SettingsAbout : Page {
 
+        private string Changelog;
         private string FooterVersion;
 
         public SettingsAbout() {
@@ -11,6 +13,9 @@ namespace UWP.Views {
 
             var version = Package.Current.Id.Version;
             FooterVersion = string.Format("Version {0}.{1}.{2}", version.Major, version.Minor, version.Build);
+
+            var changelog = Changelogs.CurrentChangelog;
+            Changelog = Changelogs.FormatChangelog(changelog);
         }
 
     }
