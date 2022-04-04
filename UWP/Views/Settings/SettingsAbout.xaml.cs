567d88f693de6c86d470e9f2a54a8d7cb4f21111
Ismael <ismael.em@outlook.com>
2022-04-04 23:28:41 +0200

    Settings: Installation folder btn

--- UWP/Views/Settings/SettingsAbout.xaml.cs
+++ UWP/Views/Settings/SettingsAbout.xaml.cs
@@ -1,12 +1,14 @@
-﻿using UWP.Core.Constants;
+﻿using System;
+using UWP.Core.Constants;
 using Windows.ApplicationModel;
+using Windows.System;
 using Windows.UI.Xaml.Controls;
 
 namespace UWP.Views {
     public sealed partial class SettingsAbout : Page {
 
         private string Changelog;
-        private string FooterVersion;
+        private string FooterVersion { get; set; }
 
         public SettingsAbout() {
             this.InitializeComponent();
@@ -18,5 +20,9 @@ namespace UWP.Views {
             Changelog = Changelogs.FormatChangelog(changelog);
         }
 
+        private async void OpenInstallationFolder_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
+            var path = Windows.Storage.ApplicationData.Current.LocalFolder;
+            await Launcher.LaunchFolderAsync(path);
+        }
     }
 }
