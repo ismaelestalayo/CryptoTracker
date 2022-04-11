7c8ea4600c050fa9c8fe3a8663945d730937baf4
Ismael <ismael.em@outlook.com>
2022-04-11 12:18:48 +0200

    Settings: startup task

--- UWP/Views/Settings/SettingsGeneral.xaml.cs
+++ UWP/Views/Settings/SettingsGeneral.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UWP.Core.Constants;
-using UWP.Helpers;
 using UWP.Shared.Constants;
-using UWP.Shared.Helpers;
+using Windows.ApplicationModel;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
 
 namespace UWP.Views {
     public sealed partial class SettingsGeneral : Page {
@@ -17,11 +18,19 @@ namespace UWP.Views {
             Loaded += SettingsGeneral_Loaded;
         }
 
-        private void SettingsGeneral_Loaded(object sender, RoutedEventArgs e) {
+        private async void SettingsGeneral_Loaded(object sender, RoutedEventArgs e) {
             vm.AutoRefresh = App._LocalSettings.Get<string>(UserSettings.AutoRefresh);
             vm.Currency = App._LocalSettings.Get<string>(UserSettings.Currency);
             vm.StartupPage = App._LocalSettings.Get<string>(UserSettings.StartupPage).Replace("/", "");
             vm.Timespan = App._LocalSettings.Get<string>(UserSettings.Timespan);
+
+            var task = await StartupTask.GetAsync("CE0D17DE-AC9B-4B2D-AE14-FFACDE33BF00");
+            vm.CanOpenInLogin = new[] { StartupTaskState.Disabled, StartupTaskState.Enabled }.Contains(
+                task.State);
+            vm.OpenInLogin = new[]{ StartupTaskState.Enabled, StartupTaskState.EnabledByPolicy}.Contains(
+                task.State);
+
+            StartupToggleSwitch.Toggled += Startup_Toggled;
         }
 
         // ###############################################################################################
@@ -30,5 +39,23 @@ namespace UWP.Views {
 
         private void Timespan_SelectionChanged(object sender, SelectionChangedEventArgs e)
             => App._LocalSettings.Set(UserSettings.Timespan, vm.Timespan);
+
+        private async void Startup_Toggled(object sender, RoutedEventArgs e) {
+            var startupTask = await StartupTask.GetAsync("CE0D17DE-AC9B-4B2D-AE14-FFACDE33BF00");
+
+            if (((ToggleSwitch)sender).IsOn) {
+                var result = await startupTask.RequestEnableAsync();
+                if (result != StartupTaskState.Enabled) {
+                    vm.OpenInLogin = false;
+                    vm.CanOpenInLogin = false;
+                }
+            }
+            else {
+                startupTask.Disable();
+            }
+        }
+
+        private async void StartupInfoBarBtn_Click(object sender, RoutedEventArgs e)
+            => await Launcher.LaunchUriAsync(new Uri("ms-settings:startupapps"));
     }
 }
